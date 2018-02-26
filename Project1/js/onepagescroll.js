44e14615d48a735cc96ddcdcf2f8464b3aa29a44
Andrii <Andru-Besaga@yandex.ru>
2018-02-26 21:50:36 +0100

    commit burger  one page

--- Project1/js/onepagescroll.js
+++ Project1/js/onepagescroll.js
@@ -1,3 +1,30 @@
+const display = $('.maincontent');
+const sections = $('.section');
+
+const performanceTransition = sectionEq => {
+    const position = (sectionEq * 100) + '%';
+    
+    display.css({
+        'transform': `translateY(${position})`,
+        '-webkit-transform': `translateY(${position})`
+    }); 
+
+    sections.eq(sectionEq).addClass('active');
+     .siblings().remoweClass('active');
+}
+
+
+
 $('.wrapper').on('wheel', e => {
-    console.log('wheellee');
+    const deltaY = e.originalEvent.deltaY;
+    const activeSection = sections.filter('active');
+    const nextSection = activeSection.next();
+    if (deltaY > 0)  { //scrool down
+        
+        performanceTransition(nextSection.index());
+    }
+
+    if (deltaY < 0)  { //scrool up
+        console.log('вверх');
+    }
 });
