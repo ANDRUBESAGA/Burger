d8142b9a55ae447bbbcb6feeeb73ec423b892339
Andrii <Andru-Besaga@yandex.ru>
2018-02-28 19:54:02 +0100

    commit olmoust finished onepagescroll

--- Project1/js/onepagescroll.js
+++ Project1/js/onepagescroll.js
@@ -1,30 +1,50 @@
 const display = $('.maincontent');
 const sections = $('.section');
 
+let inScroll = false ;
+
+const swithMenuActiveClass = sectionEq => {
+    $('.fixed-menu__item').eq(sectionEq).addClass('active')
+        .siblings().removeClass('active');
+}
+
+
 const performanceTransition = sectionEq => {
-    const position = (sectionEq * 100) + '%';
-    
+    if (inScroll) return
+    inScroll = true
+
+    const position = (sectionEq * -100) + '%';
+
     display.css({
         'transform': `translateY(${position})`,
         '-webkit-transform': `translateY(${position})`
     }); 
 
     sections.eq(sectionEq).addClass('active')
-     .siblings().removeClass('active');
+        .siblings().removeClass('active');
+
+
+    setTimeout(() => {
+        inScroll = false;
+        swithMenuActiveClass(sectionEq);
+    }, 1300)
 }
 
 
 
 $('.wrapper').on('wheel', e => {
     const deltaY = e.originalEvent.deltaY;
-    const activeSection = sections.filter('active');
+    const activeSection = sections.filter('.active');
     const nextSection = activeSection.next();
-    if (deltaY > 0)  { //scrool down
+    const prevSection = activeSection.prev();
+
+    if (deltaY > 0 && nextSection.length)  { //scrool down
         
         performanceTransition(nextSection.index());
     }
 
-    if (deltaY < 0)  { //scrool up
-        console.log('вверх');
+    if (deltaY < 0 && prevSection.length)  { //scrool up
+
+        performanceTransition(prevSection.index());
     }
 });
